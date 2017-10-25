e46f2e21696a417252d7994f96dcdbdc2908feb5
Vadim Lavorchuk <lavorchukvadim@gmail.com>
2017-10-25 17:41:57 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 window.onload = function() {
-    drawTable(5);
-    drawSpiral(5);
-
+    drawTable(15);
+    drawSpiral(15);
 }
 
 function drawTable(number) {
@@ -24,44 +23,55 @@ function drawSpiral(number) {
     let numbers = getNumbers(number, 6),
         numberOfDirections = number + number - 1,
         table = document.querySelectorAll('tr'),
-        down = [0, 4, 8, 12, 16, 20, 24, 28, 32],
-        right = [1, 5, 9, 13, 17, 21, 25, 29, 33],
-        up = [2, 6, 10, 14, 18, 22, 26, 30, 34],
-        left = [3, 7, 11, 15, 19, 23, 27, 31, 35],
-        time = 1000
-
+        down = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40],
+        right = [1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41],
+        up = [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42],
+        left = [3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43],
+        time = 100,
+        test = new Array();
 
     for (let i = 0; i < numberOfDirections; i++) {
-
-        (function(index) {
-            setTimeout(function(index) {
-                if (down.includes(i)) {
-                    let step = down.indexOf(i);
-                    for (let cell = step; cell < table.length; cell++) {
-                        table[cell].cells[step].className = "";
-                    }
-                }
-                if (right.includes(i)) {
-                    let step = right.indexOf(i);
-                    for (let cell = 0; cell < table.length; cell++) {
-                        table[table.length - 1 - step].cells[cell].className = "";
-                    }
-                }
-                if (up.includes(i)) {
-                    let step = up.indexOf(i);
-                    for (let cell = table.length - 1; cell > 0; cell--) {
-                        table[cell].cells[table.length - 1 - step].className = "";
-                    }
-                }
-                if (left.includes(i)) {
-                    let step = left.indexOf(i);
-                    for (let cell = table.length - 1; cell > 0; cell--) {
-                        table[0 + step].cells[cell].className = "";
-                    }
-                }
-            }, time * index);
-        })(i);
+        if (down.includes(i)) {
+            let step = down.indexOf(i);
+            for (let cell = step; cell < table.length - step; cell++) {
+                test.push(table[cell].cells[step]);
+            }
+        }
+        if (right.includes(i)) {
+            let step = right.indexOf(i);
+            for (let cell = step + step + 1; cell < table.length; cell++) {
+                test.push(table[table.length - 1 - step].cells[cell - step]);
+            }
+        }
+        if (up.includes(i)) {
+            let step = up.indexOf(i);
+            for (let cell = table.length - 2 - step; cell >= step; cell--) {
+                test.push(table[cell].cells[table.length - 1 - step]);
+            }
+        }
+        if (left.includes(i)) {
+            let step = left.indexOf(i),
+                count = number - 2 - step - step;
+            for (let cell = count; cell > 0; cell--) {
+                // table[0 + step].cells[cell].className = "";
+                test.push(table[step].cells[step + cell]);
+            }
+        }
     }
+    draw(test, numbers, time);
+}
+
+function draw(arrayOfElements, numbers, time) {
+    arrayOfElements.map(function(elem, index) {
+        (function(index) {
+            setTimeout(function() {
+                elem.className = "";
+                let text = document.createTextNode(numbers[index]);
+                elem.appendChild(text);
+            }, time * index);
+        })(index)
+    })
+
 }
 
 function getNumbers(number, skip) {
